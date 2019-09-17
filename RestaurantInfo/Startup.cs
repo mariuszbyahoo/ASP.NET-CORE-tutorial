9d7952dcb4b5b75f22a85373cb89903cf161ff4d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-17 21:47:08 +0200

    Serving the files from the node_modules Directory

--- RestaurantInfo/Startup.cs
+++ RestaurantInfo/Startup.cs
@@ -46,7 +46,8 @@ namespace RestaurantInfo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -61,6 +62,7 @@ namespace RestaurantInfo
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseNodeModules(env);
             app.UseCookiePolicy();
 
             app.UseMvc();
