aba0f553a6c51e87fdfa8d1b9008d927ad21915c
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-17 07:34:50 +0200

    Adding a DbContext class

---
+++ RestaurantInfo.Data/RestaurantInfoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantInfo.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantInfo.Data
+{
+    class RestaurantInfoDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
