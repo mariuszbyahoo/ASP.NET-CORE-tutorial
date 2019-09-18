fdd7eb8305bd4e5398f2ef8d08a9725f1f97027e
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-18 16:27:53 +0200

    Logging applications messages.

--- RestaurantInfo/Pages/Restaurants/List.cshtml.cs
+++ RestaurantInfo/Pages/Restaurants/List.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RestaurantInfo.Core;
 using RestaurantInfo.Data;
 
@@ -15,20 +16,26 @@ namespace RestaurantInfo.Pages.Restaurants
     {
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
+
         [BindProperty(SupportsGet =true)]
         public string SearchTerm { get; set; }
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> restaurants { get; set; }
 
-        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        public ListModel(IConfiguration config, 
+            IRestaurantData restaurantData,
+            ILogger<ListModel> logger)
         {
             this.config = config;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
 
         public void OnGet()
         {
+            logger.LogInformation("Executing ListModel");
             Message = config["Message"];
             restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
